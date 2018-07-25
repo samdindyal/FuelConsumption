5e0bc0de18e2c5b7bab5f6c217e6b262ba37f762
Sam Dindyal <sam.dindyal@me.com>
2018-07-24 22:37:16 -0400

    Adds handling and calculating for fuel consumption.

--- FuelConsumption/FuelConsumptionViewController.swift
+++ FuelConsumption/FuelConsumptionViewController.swift
@@ -10,4 +10,44 @@ import UIKit
 
 class FuelConsumptionViewController: UIViewController {
     
+    @IBOutlet var distanceField: UITextField!
+    @IBOutlet var fuelField: UITextField!
+    @IBOutlet var resultLabel: UILabel!
+    
+    @IBAction func textFieldChanged(_ sender: UITextField) {
+        if calculate(),
+            result != Float.nan {
+            resultLabel.text = "\(result)"
+        } else {
+            resultLabel.text = "???"
+        }
+    }
+    
+    var distanceTravelled:Float = 0.0
+    var fuelConsumed:Float      = 0.0
+    var result: Float           = 0.0
+    
+    func fetchValues() -> Bool {
+        
+        if let distanceTravelled = Float(distanceField.text!),
+            let fuelConsumed = Float(fuelField.text!) {
+            
+            self.distanceTravelled = distanceTravelled
+            self.fuelConsumed = fuelConsumed
+            return true
+        }
+        
+        
+        return false
+    }
+    
+    func calculate() -> Bool {
+        
+        if fetchValues() {
+            self.result = 100 * self.fuelConsumed / self.distanceTravelled
+            return true
+        }
+        
+        return false
+    }
 }
