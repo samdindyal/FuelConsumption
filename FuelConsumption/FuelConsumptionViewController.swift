3231775a59e5bec682f58c3b19845f64a3680459
Sam Dindyal <sam.dindyal@me.com>
2018-07-24 22:16:29 -0400

    Creates new view controller.

---
+++ FuelConsumption/FuelConsumptionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FuelConsumptionViewController.swift
+//  FuelConsumption
+//
+//  Created by Sam Dindyal on 2018-07-24.
+//  Copyright © 2018 Sam Dindyal. All rights reserved.
+//
+
+import UIKit
+
+class FuelConsumptionViewController: UIViewController {
+    
+}
